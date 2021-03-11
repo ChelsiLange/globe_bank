78ff5d4d85833daad401ad462ba6bf54b7b74c5e
ChelsiLange <langechelsi@gmail.com>
2021-03-11 07:36:08 -0500

    Completed Chapter Two

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -3,15 +3,14 @@
 <?php $page_title = 'Pages'; ?>
 <?php include(SHARED_PATH. '/staff_header.php'); ?>
 
-
 <?php $id = $_GET['id'] ?? '1'; ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">Back</a>
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">Back</a>
 
-  <div>
-    <?php echo "Subject ID Number: ".h($id); ?>
+  <div class="page show">
+    <?php echo "Page ID Number: ".h($id); ?>
   </div>
 
 </div>
