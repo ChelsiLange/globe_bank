d3dd0df0cbb11bb5dc148b75e67ca3f617fb1198
ChelsiLange <langechelsi@gmail.com>
2021-04-14 14:36:21 -0400

    Finishes Chapter 7

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -6,22 +6,28 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/pages/index.php'));
 }
 $id = $_GET['id'];
-$menu_name = '';
-$position = '';
-$visible = '';
 
 if(is_post_request()) {
 
   // Handle form values sent by new.php
   
-  $menu_name = $_POST['menu_name'] ?? '';
-  $position = $_POST['position'] ?? '';
-  $visible = $_POST['visible'] ?? '';
-  
-  echo "Form parameters<br>";
-  echo "Menu name: " .$menu_name. "<br>";
-  echo "Position: " .$position. "<br>";
-  echo "Visible: " .$visible. "<br>";
+  $page = [];
+  $page['id'] = $id;
+  $page['subject_id'] = $_POST['subject_id'] ?? '';
+  $page['menu_name'] = $_POST['menu_name'] ?? '';
+  $page['position'] = $_POST['position'] ?? '';
+  $page['visible'] = $_POST['visible'] ?? '';
+  $page['content'] = $_POST['content'] ?? '';
+
+  $result = update_page($page);
+  redirect_to(url_for('/staff/pages/show.php?id=' . $id));
+  }
+  else {
+    $page = find_page_by_id($id);
+
+    $page_set = find_all_pages();
+    $page_count = mysqli_num_rows($page_set);
+    mysqli_free_result($page_set);
   }
   
 ?>
@@ -38,13 +44,21 @@ if(is_post_request()) {
     <form action="<?php echo url_for('/staff/pages/edit.php?id=' .h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo h($page['menu_name']); ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1" <?php if($position == "1") { echo "selected"; } ?>>1</option>
+          <?php
+          for($i=1; $i <= $page_count; $i++) {
+                echo "<option value=\"{$i}\"";
+                if($page["position"] == $i) { 
+                  echo "selected";
+                  }
+                  echo ">{$i}</option>";
+                }
+          ?>
           </select>
         </dd>
       </dl>
@@ -52,7 +66,7 @@ if(is_post_request()) {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1" <?php if($visible == "1") {echo "checked"; } ?>/>
+          <input type="checkbox" name="visible" value="1" <?php if($page['visible'] == "1") {echo "checked"; } ?>/>
         </dd>
       </dl>
       <div id="operations">
--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,16 +1,51 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php $page_title = 'Pages'; ?>
+<?php 
+$id = $_GET['id'] ?? '1';
+
+$page = find_page_by_id($id);
+
+?>
+
+<?php $page_title = 'Show Page'; ?>
 <?php include(SHARED_PATH. '/staff_header.php'); ?>
 
-<?php $id = $_GET['id'] ?? '1'; ?>
+
 
 <div id="content">
 
   <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">Back</a>
 
   <div class="page show">
-    <?php echo "Page ID Number: ".h($id); ?>
+    
+    <h1>Page: <?php echo h($page['menu_name']); ?></h1>
+
+    <div class="attributes">
+      <?php $subject = find_subject_by_id($page['subject_id']); ?>
+      <dl>
+        <dt>Subject</dt>
+        <dd><?php echo h($subject['menu_name']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><?php echo h($page['menu_name']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd><?php echo h($page['position']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd><?php echo $page['visible'] == '1' ? 'true' : 'false'; ?></dd>
+      </dl>
+      <dl>
+        <dt>Content</dt>
+        <dd><?php echo h($page['content']); ?></dd>
+      </dl>
+    </div>
+    
+
+
   </div>
 
 </div>
