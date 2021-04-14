9fdfc115fba6323504ed4e8b0805beac8159895b
ChelsiLange <langechelsi@gmail.com>
2021-04-14 16:00:19 -0400

    Completes Chapter 8, next up is the challenge

--- private/initialize.php
+++ private/initialize.php
@@ -24,8 +24,11 @@ ob_start();  // output buffering on
   require_once('functions.php');
   require_once('database.php');
   require_once('query_functions.php');
+  require_once('validation_functions.php');
 
 
   $db = db_connect();
+  $errors = [];
+
 
 ?>
